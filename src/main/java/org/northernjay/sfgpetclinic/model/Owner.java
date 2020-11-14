d35dbc0104d14f347ce66575d7d1d8f329468141
luorenjie <lrj193927@gmail.com>
2020-11-14 18:06:22 -0500

    add POJOs. Closes #2.

---
+++ src/main/java/org/northernjay/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package org.northernjay.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/org/northernjay/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package org.northernjay.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
