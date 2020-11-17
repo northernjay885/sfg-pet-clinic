fbeae63add3074c5165a69f50d0d8d2d53a3ba18
luorenjie <lrj193927@gmail.com>
2020-11-16 21:11:00 -0500

    list vets on index. Closes #16

--- pet-clinic-web/src/main/java/org/northernjay/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/northernjay/sfgpetclinic/controllers/VetController.java
@@ -1,15 +1,24 @@
 package org.northernjay.sfgpetclinic.controllers;
 
+import org.northernjay.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"", "/", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/vets/index", "/vets/index.html"})
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
