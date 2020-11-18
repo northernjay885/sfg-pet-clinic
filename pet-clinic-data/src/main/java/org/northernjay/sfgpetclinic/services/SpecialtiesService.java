b84e16d399d75dbe610e33d51b74d444037ea82b
luorenjie <lrj193927@gmail.com>
2020-11-18 10:10:54 -0500

    add specialties service impl. # Closes #32

---
+++ pet-clinic-data/src/main/java/org/northernjay/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package org.northernjay.sfgpetclinic.services;
+
+import org.northernjay.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long>{
+
+}
