b12bb7aff51a1d8eff5fa7c2dc2979cc6f65f027
luorenjie <lrj193927@gmail.com>
2020-11-20 15:27:27 -0500

    update visit entity. Closes #39

--- pet-clinic-data/src/main/java/org/northernjay/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/org/northernjay/sfgpetclinic/model/Pet.java
@@ -1,5 +1,8 @@
 package org.northernjay.sfgpetclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
@@ -20,6 +23,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -51,5 +57,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
