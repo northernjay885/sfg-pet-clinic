64babf8d18bfda65a8fd4e5d2b1f747d615ccd0d
luorenjie <lrj193927@gmail.com>
2020-12-13 09:28:54 -0500

    fix add/update pet. Closes #57, Closes #64

--- pet-clinic-web/src/main/java/org/northernjay/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/org/northernjay/sfgpetclinic/controllers/PetController.java
@@ -68,6 +68,7 @@ public class PetController {
             model.addAttribute("pet", pet);
             return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
         } else {
+            pet.setOwner(owner);
             Pet savedPet = petService.save(pet);
             return "redirect:/owners/" + owner.getId();
         }
@@ -88,6 +89,7 @@ public class PetController {
             return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
         } else {
             owner.getPets().add(pet);
+            pet.setOwner(owner);
             petService.save(pet);
             return "redirect:/owners/" + owner.getId();
         }
