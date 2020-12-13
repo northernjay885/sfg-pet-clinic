a5d4c4d06be861a5b4f0c81244147a57da46654f
luorenjie <lrj193927@gmail.com>
2020-12-12 20:44:44 -0500

    add builder pattern to PetType. #57

--- pet-clinic-data/src/main/java/org/northernjay/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/org/northernjay/sfgpetclinic/model/PetType.java
@@ -7,12 +7,26 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @EqualsAndHashCode(callSuper = true)
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
     @Column(name = "name")
     private String name;
+
+    @Override
+    public String toString() {
+        return name;
+    }
 
 }
